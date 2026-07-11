9d9360115f1baf78a189674826c02c9b39f3331f
Arellys Villena <arellysvgamboa@gmail.com>
2026-07-11 11:41:48 -0500

    feat: creación de la clase plan academico

---
+++ src/planes/PlanAcademico.java
@@ -0,0 +1,12 @@
+package planes;
+
+public class PlanAcademico extends PlanDatos {
+
+    public PlanAcademico() {
+        super("Academico");
+    }
+
+    @Override
+    public void setTasa() {
+    }
+}
