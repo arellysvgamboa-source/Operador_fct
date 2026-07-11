a10e73451ceb6c0a47aa10ce539fcb268194c4c9
Arellys Villena <arellysvgamboa@gmail.com>
2026-07-11 11:35:02 -0500

    feat: creación de la clase plan negocio

---
+++ src/planes/PlanNegocio.java
@@ -0,0 +1,12 @@
+package planes;
+
+public class PlanNegocio extends PlanDatos {
+
+    public PlanNegocio() {
+        super("Negocio");
+    }
+
+    @Override
+    public void setTasa() {
+    }
+}
