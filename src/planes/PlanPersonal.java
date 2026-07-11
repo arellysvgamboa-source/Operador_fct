8e5a796c36753eee53904409efdbc203cfeca450
Arellys Villena <arellysvgamboa@gmail.com>
2026-07-11 11:29:14 -0500

    feat: creación de la clase plan personal

---
+++ src/planes/PlanPersonal.java
@@ -0,0 +1,12 @@
+package planes;
+
+public class PlanPersonal extends PlanDatos {
+
+    public PlanPersonal() {
+        super("Personal");
+    }
+
+    @Override
+    public void setTasa() {
+    }
+}
